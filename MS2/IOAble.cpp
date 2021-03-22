5b82455acf8d4652d474eb94cb9ae5ff86268240
Genne23v <59734889+Genne23v@users.noreply.github.com>
2021-03-22 14:27:58 -0400

    ms2 test

---
+++ MS2/IOAble.cpp
@@ -0,0 +1,53 @@
+/* Citation and Sources...
+Final Project Milestone 2
+Module: IOAble
+Filename: IOAble.cpp
+Version 1.0
+Author	Wonkeun No
+Revision History
+-----------------------------------------------------------
+Date      Reason
+2020/3/22  Preliminary release
+2020/?/? Debugged DMA
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code
+that my professor provided to complete my workshops and assignments.
+-----------------------------------------------------------*/
+#ifndef __IOABLE_H__
+#define __IOABLE_H__
+#include "IOAble.h"
+
+using namespace std;
+namespace sdds 
+{
+	/*ostream& IOAble::csvWrite(ostream& os) 
+	{
+		
+		return os;
+	}*/
+	/*istream& IOAble::csvRead(istream& is)
+	{
+
+		return is;
+	}
+	ostream& IOAble::write(ostream& os) const
+	{
+
+		return os;
+	}
+	istream& IOAble::read(istream& is)
+	{
+
+		return is;
+	}*/
+	ostream& operator<<(ostream& os, const IOAble& src)
+	{
+		return src.write(os);
+	}
+	istream& operator>>(istream& is, IOAble& src)
+	{
+		return src.read(is);
+	}
+	
+}
+#endif
---
+++ MS2/Menu.h
@@ -0,0 +1,36 @@
+/* Citation and Sources...
+Final Project Milestone 2
+Module: Menu
+Filename: Menu.h
+Version 1.0
+Author	Wonkeun No
+Revision History
+-----------------------------------------------------------
+Date      Reason
+2020/3/22  Preliminary release
+2020/?/?  Debugged DMA
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code
+that my professor provided to complete my workshops and assignments.
+-----------------------------------------------------------*/
+#ifndef SDDS_MENU_H_
+#define SDDS_MENU_H_
+#include <iostream>
+namespace sdds 
+{
+	class Menu
+	{
+		char* m_text;
+		int m_noOfsel;
+		void display()const;
+	public:
+		Menu(const char* MenuContent, int NoOfSelection);
+		virtual ~Menu();
+		Menu(const Menu& src);
+		Menu& operator=(const Menu& src);
+		int& operator>>(int& Selection);
+	};
+
+}
+#endif // !SDDS_MENU_H_
+
