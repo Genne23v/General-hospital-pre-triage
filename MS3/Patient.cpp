5b95cdc684ae0989481a33ef92732bdcf49a2288
Genne23v <59734889+Genne23v@users.noreply.github.com>
2021-03-27 01:24:55 -0400

    missing cstring lib

--- MS3/Patient.cpp
+++ MS3/Patient.cpp
@@ -16,6 +16,7 @@ that my professor provided to complete my workshops and assignments.
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <string>
+#include <cstring>
 #include <iomanip>
 #include "Patient.h"
 #include "Time.h"
